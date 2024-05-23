2b8e98f538d8c30a7fc0ff95ec61e391a7c6771a
ArtHard <1artem1nazarov1@gmail.com>
2024-05-23 18:57:07 +0300

    fix svg task / add new functions

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,4 +1,9 @@
 import svgSprite from "gulp-svg-sprite";
+import svgMin from "gulp-svgmin";
+import cheerio from "gulp-cheerio";
+import path from "path";
+
+const isColor = (file) => file.path.startsWith(path.resolve('src/img/icons/static'));
 
 export const sprite = () => {
 	return app.gulp.src(`${app.path.src.svgicons}`)
@@ -8,16 +13,27 @@ export const sprite = () => {
 				message: "Error: <%= error.message %>"
 			})
 		))
+		.pipe(svgMin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(app.plugins.if(file => !isColor(file), cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: { xmlMode: true }
+		})))
+		.pipe(app.plugins.replace('&gt;', '>'))
 		.pipe(svgSprite({
 			mode: {
-				stack: {
-					sprite: `../icons.svg`,
-					example: false
+				symbol: {
+					sprite: "../sprite.svg"
 				}
-			},
+			}
 		}))
-		.pipe(app.gulp.dest(`${app.path.build.images}`))
-		.pipe(app.gulp.src(app.path.src.interactiveicons))
 		.pipe(app.gulp.dest(app.path.build.images))
 		.pipe(app.plugins.browsersync.stream());
 }
